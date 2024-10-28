67534be5bda908c60fef46b6e3f6bbebf6bec74a
Bruno-Fernands <111819744+Bruno-Fernands@users.noreply.github.com>
2024-10-28 20:13:01 -0300

    Add files via upload

---
+++ Postman/cypress.config.js
@@ -0,0 +1,34 @@
+{
+	"info": {
+		"_postman_id": "64031307-cfb8-4c8c-a980-8598231c78e4",
+		"name": "Aula 1 - Pokemon",
+		"schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+		"_exporter_id": "39345557"
+	},
+	"item": [
+		{
+			"name": "Localiza pokemon com sucesso",
+			"request": {
+				"method": "GET",
+				"header": []
+			},
+			"response": []
+		},
+		{
+			"name": "Não localiza pokemon (não existe)",
+			"request": {
+				"method": "GET",
+				"header": []
+			},
+			"response": []
+		},
+		{
+			"name": "Endpoint falho localiza pokemon",
+			"request": {
+				"method": "GET",
+				"header": []
+			},
+			"response": []
+		}
+	]
+}
